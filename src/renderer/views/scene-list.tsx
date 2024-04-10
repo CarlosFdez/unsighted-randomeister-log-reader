27433dcfde9fc43e29f3c71e7b0d2c9f3806f054
Carlos Fernandez <cfern1990@gmail.com>
2024-04-10 18:06:49 -0400

    Add scene list filter input

--- src/renderer/views/scene-list.tsx
+++ src/renderer/views/scene-list.tsx
@@ -1,18 +1,40 @@
 import { SceneData, useScenes } from "../hooks";
-import { Link, ScrollRestoration, useMatch } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
 import { css } from "@emotion/react";
 import { useDrag } from "react-dnd";
 import { SidebarStyle } from "@renderer/components/Sidebar";
+import { FormEvent, useMemo, useState } from "react";
+import * as R from "remeda";
+import MiniSearch from "minisearch";
 
 export function SceneList() {
     const scenes = useScenes();
     const { sceneName } = useMatch("/scenes/:sceneName")?.params ?? {};
+    const [filter, setFilter] = useState("");
+    const search = useMemo(() => {
+		const search = new MiniSearch({
+            idField: "name",
+			fields: ["name"],
+			searchOptions: {
+                prefix: true,
+				fuzzy: 0.2,
+			},
+		});
+		search.addAll(Object.values(scenes));
+		return search;
+	}, [scenes]);
+
+    const results = filter ? search.search(filter) : null;
+    const visibleScenes = Object.values(results ? R.pick(scenes, results.map((r) => r.id)) : scenes);
+
     return (
         <div css={SceneListSidebarStyle}>
-            <ScrollRestoration />
-            {Object.values(scenes).map((s) =>
-                <SceneListEntry key={s.name} scene={s} selected={sceneName === s.name}/>
-            )}
+            <input type="text" placeholder="Filter" value={filter} onInput={(evt: FormEvent<HTMLInputElement>) => setFilter(evt.currentTarget.value)} />
+            <div css={SceneListEntryListStyle}>
+                {visibleScenes.map((s) =>
+                    <SceneListEntry key={s.name} scene={s} selected={sceneName === s.name}/>
+                )}
+            </div>
         </div>
     );
 }
@@ -54,6 +76,18 @@ const SceneListSidebarStyle = css`
     ${SidebarStyle}
     flex: 0 0 min-content;
     white-space: nowrap;
+    display: flex;
+    flex-direction: column;
+
+    input {
+        border: none;
+        border-bottom: 1px solid #666;
+        padding: 0.25rem 0.375rem;
+    }
+`;
+
+const SceneListEntryListStyle = css`
+    overflow: auto;
 `;
 
 const SceneListEntryStyle = css`
@@ -62,7 +96,7 @@ const SceneListEntryStyle = css`
     cursor: pointer;
     color: unset;
     text-decoration: unset;
-    padding: 4px 6px;
+    padding: 0.25rem 0.375rem;
     &:hover, &.selected {
         background-color: rgba(255, 255, 255, 0.8);
     }
